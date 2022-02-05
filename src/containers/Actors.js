82f7f541cc79f8fabf01425c56c7520a8b2ce768
hafizbali315 <hafizbali315@gmail.com>
2022-02-05 22:11:28 +0500

    Actors UI

--- src/containers/Actors.js
+++ src/containers/Actors.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import Actor1 from '../assets/actors/Actor3.png'
 
 const Actors = () => {
 	return (
-		<div>
-			<h1>Actors</h1>
+		<div className="actors">
+			<div className="row gx-5">
+				<div className="col-lg-4 single_actor active">
+					<div className="img_container">
+						<img src={Actor1} alt="Actor" />
+					</div>
+					<h6>Anna</h6>
+				</div>
+				<div className="col-lg-4 single_actor">
+					<div className="img_container">
+						<img src={Actor1} alt="Actor" />
+					</div>
+					<h6>Anna</h6>
+				</div>
+				<div className="col-lg-4 single_actor">
+					<div className="img_container">
+						<img src={Actor1} alt="Actor" />
+					</div>
+					<h6>Anna</h6>
+				</div>
+				<div className="col-lg-4 single_actor">
+					<div className="img_container">
+						<img src={Actor1} alt="Actor" />
+					</div>
+					<h6>Anna</h6>
+				</div>
+				<div className="col-lg-4 single_actor">
+					<div className="img_container">
+						<img src={Actor1} alt="Actor" />
+					</div>
+					<h6>Anna</h6>
+				</div>
+				<div className="col-lg-4 single_actor">
+					<div className="img_container">
+						<img src={Actor1} alt="Actor" />
+					</div>
+					<h6>Anna</h6>
+				</div>
+			</div>
 		</div>
 	)
 }
