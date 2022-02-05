98a0dbfa3dd4bc8d0f7b56736edd14441727c562
hafizbali315 <hafizbali315@gmail.com>
2022-02-05 20:51:03 +0500

    Create-video Page Image Section and Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CreateVideo from './containers/CreateVideo'
 import Layout from './containers/Layout'
+import Actors from './containers/Actors'
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
 				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route index element={<CreateVideo />} />
-						<Route path="/create-video" element={<CreateVideo />} />
+					<Route element={<Layout />}>
+						<Route path="/create-video" element={<CreateVideo />}>
+							<Route index element={<Actors />} />
+							<Route path="actor" element={<Actors />} />
+							<Route path="voice" element={<Actors />} />
+							<Route path="alignment" element={<Actors />} />
+							<Route path="background" element={<Actors />} />
+						</Route>
 					</Route>
 				</Routes>
 			</Router>
--- src/containers/CreateVideo.js
+++ src/containers/CreateVideo.js
@@ -1,8 +1,72 @@
+import { RiArrowDropDownLine } from 'react-icons/ri'
+import Background2 from '../assets/backgrounds/Background4.png'
+
+import Actor2 from '../assets/actors/Actor1.png'
+import { Link, Outlet } from 'react-router-dom'
+
 const CreateVideo = () => {
 	return (
-		<div>
-			<div className="create_video">CreateVideo</div>
-		</div>
+		<>
+			<div className="create_video">
+				<div className="main_header">
+					<h1>
+						Saying Hi to my customers <RiArrowDropDownLine className="icon" />
+					</h1>
+
+					<div className="buttons">
+						<button>Cancel</button>
+						<button>Save</button>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-lg-6">
+						<div className="image_section">
+							<div className="img_container">
+								<img className="background_img" src={Background2} alt="BackgroundImg" />
+								<img className="actor_img" src={Actor2} alt="ActorImg" />
+							</div>
+
+							<div className="text_area">
+								<textarea placeholder="Type or paste your videoscript here. You can also request a translation of an English script to any of 27 other languages"></textarea>{' '}
+								<button>Listen</button>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-6">
+						<div className="setting_header">
+							<ul>
+								<li>
+									<Link className="link" to="actor">
+										Actor
+									</Link>
+								</li>
+
+								<li>
+									<Link className="link" to="voice">
+										Voice
+									</Link>
+								</li>
+
+								<li>
+									<Link className="link" to="alignment">
+										Algnment
+									</Link>
+								</li>
+
+								<li>
+									<Link className="link" to="background">
+										Background
+									</Link>
+								</li>
+							</ul>
+						</div>
+
+						<Outlet />
+					</div>
+				</div>
+			</div>
+		</>
 	)
 }
 
