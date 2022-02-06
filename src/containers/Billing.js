ff8ede7a6c8b87492ebbfaee82cd985e759fa322
hafizbali315 <hafizbali315@gmail.com>
2022-02-06 06:43:50 +0500

    Billing Page

--- src/containers/Billing.js
+++ src/containers/Billing.js
@@ -1,5 +1,47 @@
+import { VscFilePdf } from 'react-icons/vsc'
+
 const Billing = () => {
-	return <div>Billings</div>
+	return (
+		<div className="billing">
+			<div className="table table-borderless">
+				<thead>
+					<tr>
+						<th style={{ width: '250px' }}>ReferEnce ID</th>
+						<th style={{ width: '200px' }}>DATE</th>
+						<th style={{ width: '200px' }}>AMOUNT</th>
+						<th style={{ width: '200px' }}>Invoice</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr>
+						<td>4571222f6rthswfg9981fr55</td>
+						<td>7/12/2020</td>
+						<td>$26</td>
+						<td>
+							<VscFilePdf className="icon" />
+						</td>
+					</tr>
+					<tr>
+						<td>4571222f6rthswfg9981fr55</td>
+						<td>7/12/2020</td>
+						<td>$26</td>
+						<td>
+							<VscFilePdf className="icon" />
+						</td>
+					</tr>
+					<tr>
+						<td>4571222f6rthswfg9981fr55</td>
+						<td>7/12/2020</td>
+						<td>$26</td>
+						<td>
+							<VscFilePdf className="icon" />
+						</td>
+					</tr>
+				</tbody>
+			</div>
+		</div>
+	)
 }
 
 export default Billing
